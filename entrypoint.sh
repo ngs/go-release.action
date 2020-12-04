37529e9c649d3138139cd7401ed57227ca02393c
Mattias Hansson <hansson.mattias@gmail.com>
2020-12-04 15:18:17 +0100

    entrypoint.sh: Export PROJECT_NAME
    Currenty, PROJECT_NAME is used referenced in `build.sh`. However, it
    will always be empty since PROJECT_NAME is not exported in
    `entrypoint.sh`.
    
    This patch exports PROJECT_NAME so it becomes accessible in `build.sh`.

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,7 +21,7 @@ echo $EVENT_DATA | jq .
 UPLOAD_URL=$(echo $EVENT_DATA | jq -r .release.upload_url)
 UPLOAD_URL=${UPLOAD_URL/\{?name,label\}/}
 RELEASE_NAME=$(echo $EVENT_DATA | jq -r .release.tag_name)
-PROJECT_NAME=$(basename $GITHUB_REPOSITORY)
+export PROJECT_NAME=$(basename $GITHUB_REPOSITORY)
 NAME="${NAME:-${PROJECT_NAME}_${RELEASE_NAME}}_${GOOS}_${GOARCH}"
 
 if [ -z "${EXTRA_FILES+x}" ]; then
