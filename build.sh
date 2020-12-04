b9d0d01b7bd133355d56821a4697202b78602dea
Mattias Hansson <hansson.mattias@gmail.com>
2020-12-04 15:34:04 +0100

    build.sh: Configurable binary name
    The name of the build output was enforced to project name. This patch
    adds a configurable name for the build output dictated by BIN_NAME. The
    the output will default to $PROJECT_NAME$EXT if BIN_NAME is left unset.
    Note that `BUILD_FLAGS="-o $BIN_NAME"` must be used if the
    `basename $CMD_PATH` != BIN_NAME.

--- build.sh
+++ build.sh
@@ -10,17 +10,17 @@ ln -s $GITHUB_WORKSPACE $PROJECT_ROOT
 cd $PROJECT_ROOT
 go get -v ./...
 
-EXT=''
-
-if [ $GOOS == 'windows' ]; then
-EXT='.exe'
-fi
-
 if [ -x "./build.sh" ]; then
   OUTPUT=`./build.sh "${CMD_PATH}"`
 else
   go build ${BUILD_FLAGS} "${CMD_PATH}"
-  OUTPUT="${PROJECT_NAME}${EXT}"
+  OUTPUT="${BIN_NAME:=$PROJECT_NAME}"
+
+  if [ $GOOS == 'windows' ]; then
+    EXT="$OUTPUT.exe"
+    mv $OUTPUT $EXT
+    OUTPUT=$EXT
+  fi
 fi
 
 echo ${OUTPUT}
